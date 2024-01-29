c532615193235a8de60443acf4a189e0ac7059e0
pragatiray <93862662+pragatiray@users.noreply.github.com>
2024-01-29 13:28:40 +0000

    added tables on large screen and job list on small screen

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
+
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
+
 
 const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+// Render the template
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'pages/index.html'));
+    res.render('index', { jobs: JOBS});
 });
 
 const port = process.env.PORT || 3000;
@@ -14,4 +23,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on https://localhost:${port}`);
 });
-
