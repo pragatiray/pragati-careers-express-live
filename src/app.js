07539c6783ea2684d7340323955bbaf325a79bcb
pragatiray <93862662+pragatiray@users.noreply.github.com>
2024-01-29 13:47:05 +0000

    Added application form

--- src/app.js
+++ src/app.js
@@ -9,17 +9,26 @@ const app = express();
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.set('views', path.join(__dirname, 'pages'));
+app.set('views',path.join(__dirname, 'pages'));
 app.set('view engine', 'mustache');
 app.engine('mustache', mustacheExpress());
 
 // Render the template
+
 app.get('/', (req, res) => {
     res.render('index', { jobs: JOBS});
+});
+
+app.get('/jobs/:id', (req, res) => {
+    console.log('req.params',req.params);
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    console.log('matchedJob',matchedJob);
+    res.render('job', { job: matchedJob});
 });
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server running on https://localhost:${port}`);
-});
+    console.log(`Server running on https://localhost:${port}`)
+})
